a877503f5e43d63d8ac84bbbdf09262dbee5bb31
vaithiyanathan <115526966+Vaithiyanathant@users.noreply.github.com>
2024-05-14 18:06:22 +0530

    Time: 31 ms (80.19%), Space: 16.5 MB (89.79%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits)==0: return []
+        lst = ["","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+        if len(digits)==1: return list(lst[int(digits)-1])
+        s = ""
+        for i in digits:
+            s+=lst[int(i)-1]
+            s+=" "
+        s =  s.split()
+        res = [ ]
+        if len(digits)==2:
+            for i in s[0]:
+                for j in s[1]:
+                    res.append(str(i+j))
+        if len(digits)==3:
+            for i in s[0]:
+                for j in s[1]:
+                    for k in s[2]:
+                        res.append(str(i+j+k))
+        if len(digits)==4:
+            for i in s[0]:
+                for j in s[1]:
+                    for k in s[2]:
+                        for l in s[3]:
+                            res.append(str(i+j+k+l))
+        return res
+
+        
