49d3f6a873eaa0167140017ce717c6caa69ace5c
vaithiyanathan <115526966+Vaithiyanathant@users.noreply.github.com>
2024-05-16 13:54:12 +0530

    Time: 38 ms (87.03%), Space: 16.5 MB (57.26%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack  = [" "]
+        for i in s[::-1]:
+            if i+stack[-1]=="CD"or i+stack[-1]=="AB":
+                stack.pop()
+            else:
+                stack.append(i)
+        print(stack)
+        return len(stack)-1
+
+
+
+        
