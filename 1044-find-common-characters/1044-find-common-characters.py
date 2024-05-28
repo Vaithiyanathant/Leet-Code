2f823894fe6b5792dea1b7e92223f5e9539dd59e
vaithiyanathan <115526966+Vaithiyanathant@users.noreply.github.com>
2024-05-28 22:42:45 +0530

    Time: 44 ms (83.38%), Space: 16.7 MB (71.53%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        lst = [[0]*len(words) for _ in range(27)]
+
+        s = " ".join(words)
+        flag  = 0
+        for i in words:
+            for j in i:
+                lst[ord(j)-97][flag]+=1
+            flag+=1
+        print(lst)
+        ans =  ""
+        for k in range(len(lst)):
+            if lst[k]!=[0,0,0]:
+                l = min(lst[k])
+                ans+=(chr(k+97))*l
+        print(ans)
+        return list(ans)
