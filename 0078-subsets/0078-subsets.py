67f0a526c18b4e16daa419f8c19b1333699029db
vaithiyanathan <115526966+Vaithiyanathant@users.noreply.github.com>
2024-06-23 12:10:50 +0530

    Time: 40 ms (34.77%), Space: 16.7 MB (70.02%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        out = []
-        def sol(i,n,temp,arr):
-            if i == n:
-                out.append(temp[:])
-                return 
-            temp.append(arr[i])
-            sol(i+1,n,temp,arr)
-            temp.pop()
-            sol(i+1,n,temp,arr)
-        ind = 0
+        lst = [[]]
         n = len(nums)
-        y = []
-        
-        sol(ind,n,y,nums)
-        return out
-
-        
+        for i in range(1,2**n):
+            k = []
+            for j in range(len(nums)):
+                if (i & 1<<j):
+                    k.append(nums[j])
+            lst.append(k)
+        return lst
